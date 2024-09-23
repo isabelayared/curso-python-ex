45d8ce198647c707b986ea13787197c8a8fc698f
isabelayared <167128156+isabelayared@users.noreply.github.com>
2024-09-23 17:56:46 -0300

    Create ex02.py

---
+++ ex02.py
@@ -0,0 +1,7 @@
+n1 = int(input('Digite um número: '))
+n2 = int(input('Digite mais um número:'))
+s = n1 + n2
+# print('A soma de', n1,'e',n2, 'vale:',s)
+print('A soma dos números {} e {} vale: {}'.format(n1, n2, s))
+
+# int() float() bool() e str()
