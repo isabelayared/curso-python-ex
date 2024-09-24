7f8963330b11475ce3d6371371c4b937238b4e4b
isabelayared <167128156+isabelayared@users.noreply.github.com>
2024-09-24 13:39:20 -0300

    Create ex14.py

---
+++ ex14.py
@@ -0,0 +1,7 @@
+print("Algoritmo para calcular o consumo.")
+DIST = int(input("Distância total percorrida (Km):"))
+print(DIST)
+VOL = float(input("Volume de combustível gasto (L): "))
+print(VOL)
+CM= DIST/VOL
+print("Consumo médio=",CM,"Km/l")
